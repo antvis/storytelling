d130cf8cc13a223f96f44dd81f304df75feb343c
hustcc <i@hust.cc>
2023-10-19 16:46:25 +0800

    chore: g6 graph style

--- src/stories/contributors/data.ts
+++ src/stories/contributors/data.ts
@@ -1,64 +1,113 @@
 import { Octokit } from '@octokit/core';
 
-
-
 /**
  * https://docs.github.com/en/rest/repos/repos?apiVersion=2022-11-28#list-repository-contributors
  * @param accessKey 
  * @param owner 
  * @param repo 
  */
-export const getContributors = async (accessKey: string, owner: string, repo: string) => {
+export const getContributors = async (accessKey: string | undefined, owner: string, repo: string) => {
   const octokit = new Octokit({
     auth: accessKey
   });
 
-  return await octokit.request('GET /repos/{owner}/{repo}/contributors', {
-    owner,
-    repo,
-    per_page: 100,
-    headers: {
-      'X-GitHub-Api-Version': '2022-11-28'
+  let page = 1;
+  let result = [];
+
+  while(true) {
+    const r = await octokit.request('GET /repos/{owner}/{repo}/contributors', {
+      owner,
+      repo,
+      per_page: 100,
+      page,
+      headers: {
+        'X-GitHub-Api-Version': '2022-11-28'
+      }
+    });
+
+    if (r.data.length === 0) {
+      return result;
+    } else {
+      result.push(...r.data);
+      page++;
     }
-  });
+  }
 }
 
-export const Data = {
-  nodes: [
-    { id: 'node0', size: 50 },
-    { id: 'node1', size: 30 },
-    { id: 'node2', size: 30 },
-    { id: 'node3', size: 30 },
-    { id: 'node4', size: 30, isLeaf: true },
-    { id: 'node5', size: 30, isLeaf: true },
-    { id: 'node6', size: 15, isLeaf: true },
-    { id: 'node7', size: 15, isLeaf: true },
-    { id: 'node8', size: 15, isLeaf: true },
-    { id: 'node9', size: 15, isLeaf: true },
-    { id: 'node10', size: 15, isLeaf: true },
-    { id: 'node11', size: 15, isLeaf: true },
-    { id: 'node12', size: 15, isLeaf: true },
-    { id: 'node13', size: 15, isLeaf: true },
-    { id: 'node14', size: 15, isLeaf: true },
-    { id: 'node15', size: 15, isLeaf: true },
-    { id: 'node16', size: 15, isLeaf: true },
-  ],
-  edges: [
-    { source: 'node0', target: 'node1' },
-    { source: 'node0', target: 'node2' },
-    { source: 'node0', target: 'node3' },
-    { source: 'node0', target: 'node4' },
-    { source: 'node0', target: 'node5' },
-    { source: 'node1', target: 'node6' },
-    { source: 'node1', target: 'node7' },
-    { source: 'node2', target: 'node8' },
-    { source: 'node2', target: 'node9' },
-    { source: 'node2', target: 'node10' },
-    { source: 'node2', target: 'node11' },
-    { source: 'node2', target: 'node12' },
-    { source: 'node2', target: 'node13' },
-    { source: 'node3', target: 'node14' },
-    { source: 'node3', target: 'node15' },
-    { source: 'node3', target: 'node16' },
-  ],
-};
+export const getReposContributors = async (accessKey: string | undefined, repos: string[]) => {
+  const ps = repos.map(repo => {
+    const [owner, repoName] = repo.split('/');
+    return getContributors(accessKey, owner, repoName);
+  });
+
+  return await Promise.all(ps);
+}
+
+/**
+ * 将数据转化成关系图数据
+ */
+export const getContributorRelation = (repos: string[], contributors: any[][]) => {
+  // repo, contributor
+  const nodes: any = [];
+  const edges: any = [];
+
+  // repo
+  repos.forEach((repo, idx) => {
+    // repo
+    nodes.push({
+      id: repo,
+
+      data: {
+        nodeType: 'repo',
+        size: 32,
+        cluster: repo,
+        // cluster: 'repo',
+        label: repo,
+        text: repo,
+        stroke: '#fff',
+      },
+    });
+
+    // repo <-> contributor
+    edges.push(...contributors[idx].map(({ login }) => ({
+      id: `${repo}-$${login}`,
+      source: repo,
+      target: login,
+      data: {
+        keyShape: {
+          strokeOpacity: 0.5,
+        }
+      },
+    })));
+  });
+
+  // contributor 合并 contributions
+  const contributionsMap = new Map();
+  contributors.forEach((contributor) => {
+    contributor.forEach(({ login, contributions }) => {
+      if (contributionsMap.has(login)) {
+        contributionsMap.set(login, { contributions: contributionsMap.get(login).contributions + contributions, single: false });
+      } else {
+        contributionsMap.set(login, { contributions, single: true });
+      }
+    });
+  });
+
+  for(let [login, { contributions, single }] of contributionsMap.entries()) {
+    nodes.push({
+      id: login,
+      
+      data: {
+        size: (Math.log2(contributions) + 4) * 2,
+        cluster: single ? 'single-user' : 'multi-user',
+        contributions,
+        label: login,
+        keyShape: {
+          fillOpacity: 0.5,
+        }
+      },
+    });
+  }
+
+  return { nodes, edges };
+}
--- src/stories/contributors/index.tsx
+++ src/stories/contributors/index.tsx
@@ -1,28 +1,41 @@
 import React, { useRef, useState, useEffect } from 'react';
-import { Space, Button, Tag, Modal, Input, InputRef } from 'antd';
+import { Space, Button, Tag, Modal, Input, InputRef, Spin } from 'antd';
 import { PlusOutlined } from '@ant-design/icons';
-import { Data } from './data';
+import { useLocalStorageState } from 'ahooks';
+import { getContributorRelation, getReposContributors } from './data';
+
 import styles from './index.module.less';
 
 const { G6V5: G6 } = window as any;
 const { Graph } = G6;
 
-const DefaultRepos = [
-  'antvis/g2',
-  'antvis/g6',
-  'antvis/l7',
-];
+const AccessKeyLocalStorageKey = '__contributor_graph_access_key__';
+const ReposLocalStorageKey = '__contributor_graph_repos__';
 
 export const Contributors: React.FC = () => {
+  const [accessKey, setAccessKey] = useLocalStorageState<string>(AccessKeyLocalStorageKey);
+  const [defaultRepos, setDefaultRepos] = useLocalStorageState<string>(ReposLocalStorageKey);
+  const [editAccessKey, setEditAccessKey] = useState(accessKey);
   const [modalOpen, setModalOpen] = useState(false);
-  const [repos, setRepos] = useState<string[]>(DefaultRepos);
+  const [loading, setLoading] = useState(false);
+  const [repos, setRepos] = useState<string[]>(JSON.parse(defaultRepos || '[]'));
+  const [contributorRelation, setContributorRelation] = useState<any>({ nodes: [], edges: [] });
+
+  // Data
+  useEffect(() => {
+    setLoading(true);
+    getReposContributors(accessKey, repos).then((data) => {
+      setContributorRelation(getContributorRelation(repos, data));
+      setLoading(false);
+    });
+  }, [repos]);
 
   // Repo input
   const [inputVisible, setInputVisible] = useState(false);
   const [inputValue, setInputValue] = useState('');
   const inputRef = useRef<InputRef>(null);
 
-    useEffect(() => {
+  useEffect(() => {
     if (inputVisible) {
       inputRef.current?.focus();
     }
@@ -31,6 +44,7 @@ export const Contributors: React.FC = () => {
   const handleClose = (removedRepo: string) => () => {
     const newRepos = repos.filter((repo) => repo !== removedRepo);
     setRepos(newRepos);
+    setDefaultRepos(JSON.stringify(newRepos));
   };
 
   const showInput = () => {
@@ -43,7 +57,9 @@ export const Contributors: React.FC = () => {
 
   const handleInputConfirm = () => {
     if (inputValue && !repos.includes(inputValue)) {
-      setRepos([...repos, inputValue]);
+      const newRepos = [...repos, inputValue];
+      setRepos(newRepos);
+      setDefaultRepos(JSON.stringify(newRepos));
     }
     setInputVisible(false);
     setInputValue('');
@@ -53,7 +69,7 @@ export const Contributors: React.FC = () => {
   const graph = useRef<any>(null);
   useEffect(() => {
     if (graph.current) {
-      return;
+      graph.current.destroy();
     }
 
     const rect = document.querySelector('#g6-container')?.getBoundingClientRect();
@@ -62,29 +78,57 @@ export const Contributors: React.FC = () => {
       container: 'g6-container',
       width: rect?.width || 1,
       height: rect?.height || 1,
-      transforms: ['transform-v4-data'],
+      theme: {
+        type: 'spec',
+        specification: {
+          node: {
+            dataTypeField: 'cluster',
+          },
+        },
+      },
+      node: {
+        keyShape: {
+          r: {
+            fields: ['size'],
+            formatter: (model: any) => {
+              return model.data.size;
+            },
+          },
+          lineWidth: 4,
+          stroke: '#000',
+          strokeOpacity: 0.2,
+        },
+        labelShape: {
+          position: {
+            fields: ['nodeType'],
+            formatter: (model: any) => model.data.nodeType === 'repo' ? 'center' : 'bottom',
+          },
+          text: {
+            fields: ['label'],
+            formatter: (model: any) => model.data.label
+          },
+        },
+        lodStrategy: {},
+      },
       layout: {
         type: 'force',
         preventOverlap: true,
         animated: true,
-        linkDistance: (d: any) => {
-          if (d.source === 'node0' || d.target === 'node0') {
-            return 200;
-          }
-          return 80;
-        },
+        maxIteration: 10000,
+        linkDistance: 80,
       },
       modes: {
         default: ['zoom-canvas', 'drag-canvas', 'drag-node', 'click-select'],
       },
-      data: Data,
+      data: contributorRelation,
     });
-  }, []);
+  }, [contributorRelation]);
 
   return (
     <div className={styles.container}>
       <div className={styles.header}>
         <div className={styles.left}>
+          <div className={styles.title}>GitHub Contributor Graph</div>
           <div className={styles.repo}>
             <Space size={[0, 'small']} wrap>
               {
@@ -124,21 +168,25 @@ export const Contributors: React.FC = () => {
       </div>
       <div className={styles.graph} id="g6-container">
         <div className={styles.power}>
-          数据来源: GitHub API, Visualizated by <a href="http://github.com/antvis/G6" target="_blank">G6</a>.
+          数据来源: GitHub API, visualized by <a href="http://github.com/antvis/G6" target="_blank">G6</a>.
         </div>
+        { loading && <div className={styles.loading}><Spin size="large" /></div> }
       </div>
 
       <Modal
         title="Edit Access Token"
         style={{ top: 36 }}
         open={modalOpen}
-        onOk={() => setModalOpen(false)}
+        onOk={(e) => {
+          setAccessKey(editAccessKey);
+          setModalOpen(false);
+        }}
         onCancel={() => setModalOpen(false)}
       >
         <p>This tool use GitHub API to retrieve repository metadata. You may see this page because you have hit the <a href="https://docs.github.com/en/rest/overview/resources-in-the-rest-api" target="_blank">GitHub API rate limit</a> .</p>
         <p>Star-history will need your <a href="https://github.com/settings/tokens" target="_blank">personal access token</a> to unlimit it. If you don't already have one, create one , and paste it into the textbox below (no scope to your personal data is needed)</p>
         <p style={{ fontWeight: 'bold' }}>Access Token (will be stored in your local storage)</p>
-        <Input placeholder="Your GitHub Access Token..." />
+        <Input placeholder="Your GitHub Access Token..." value={editAccessKey} onChange={e => setEditAccessKey(e.target.value)} />
       </Modal>
     </div>
   );
